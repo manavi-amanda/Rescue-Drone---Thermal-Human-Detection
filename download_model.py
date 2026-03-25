43a7aed2f746015b3c4f45989490e6d3070f1ad5
manavi-amanda <manaviamandahettiarachchi@gmail.com>
2026-03-25 12:51:02 +0530

    Remove unused model download script and thermal video files; add .gitignore for project files

--- download_model.py
+++
@@ -1,9 +0,0 @@
-from huggingface_hub import hf_hub_download
-
-# Download the pretrained YOLOv8 thermal human detector
-model_path = hf_hub_download(
-    repo_id="arnabdhar/YOLOv8-human-detection-thermal",
-    filename="model.pt"
-)
-
-print("Model downloaded at:", model_path)
